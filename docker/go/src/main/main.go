6c2ddafd1db00674dbfefdc831e145506190cf65
mone9610 <knemoto.prod@gmail.com>
2021-01-11 16:37:12 +0900

    modified main.go

--- docker/go/src/main/main.go
+++ docker/go/src/main/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"controller"
+	"job"
 )
 
 func main() {
 	// 無限ループでゴルーチンを起動する
 	quit := make(chan bool)
-	// go job.WriteTickerJob()
-	// go job.AutoTradingJob()
+	go job.WriteTickerJob()
+	go job.AutoTradingJob()
 	go controller.RESTAPI()
 	// 永遠に返らない
 	<-quit
