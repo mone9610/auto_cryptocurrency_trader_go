248c4186e86e4f65d8cd184620ee72c8489fe826
mone9610 <knemoto.prod@gmail.com>
2021-01-12 09:20:33 +0900

    add new feature(healthCheckPath of Go / healthCheck bash)

---
+++ tool/process_check.sh
@@ -0,0 +1,16 @@
+#goのヘルスチェック用のパスを指定する
+_URL="localhost:8000/health"
+#無限ループにてヘルスチェックを実施する
+while true
+do
+    sleep 10
+    HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X GET $_URL)
+    HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+    echo "$HTTP_STATUS"
+    if [ "$HTTP_STATUS" -eq '200' ]; then
+    echo "goのヘルスチェックに成功しました。"
+    else
+    echo "goのヘルスチェックに成功しました。失敗しました。goのプロセスを再起動します。"
+    docker-compose exec go bash -c "go run main"
+    fi
+done
